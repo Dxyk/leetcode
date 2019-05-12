486340ebab3fda7435ff99c5671788cd30e35107
Dxyk <danielkong1997@gmail.com>
2019-05-12 11:36:19 -0400

    034 binary search soln

--- DataStructure/Array/Python/034-find-first-and-last-position-of-element-in-sorted-array.py
+++ DataStructure/Array/Python/034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -11,9 +11,21 @@ class Solution:
 
         If the target is not found in the array, return [-1, -1].
 
-        >>> Solution().searchRange([5,7,7,8,8,10], 8)
+        >>> Solution().searchRange([], 1)
+        [-1, -1]
+        >>> Solution().searchRange([1], 1)
+        [0, 0]
+        >>> Solution().searchRange([1], 0)
+        [-1, -1]
+        >>> Solution().searchRange([2, 2], 3)
+        [-1, -1]
+        >>> Solution().searchRange([1, 2, 3], 3)
+        [2, 2]
+        >>> Solution().searchRange([1, 1, 1, 1, 1], 1)
+        [0, 4]
+        >>> Solution().searchRange([5, 7, 7, 8, 8, 10], 8)
         [3, 4]
-        >>> Solution().searchRange([5,7,7,8,8,10], 6)
+        >>> Solution().searchRange([5, 7, 7, 8, 8, 10], 6)
         [-1, -1]
 
         :param nums: The array of numbers
@@ -22,7 +34,55 @@ class Solution:
         """
         if not nums:
             return [-1, -1]
-        return self.brute_force(nums, target)
+        return self.binary_search_soln(nums, target)
+
+    def binary_search_soln(self, nums: List[int], target: int) -> List[int]:
+        """
+        T: O(log(n))
+        The brute force solution
+
+        :param nums: The array of numbers
+        :param target: The target value to search for
+        :return: the pair of start and end index of the target value. [-1, -1] if target not in nums
+        """
+        idx = self.binary_search(nums, target)
+        if idx == -1:
+            return [-1, -1]
+        else:
+            left = right = idx
+            while left > 0 and nums[left - 1] == target:
+                new_left = self.binary_search(nums[: left], target)
+                if new_left != -1:
+                    left = new_left
+                else:
+                    break
+            while right < len(nums) - 1 and nums[right + 1] == target:
+                new_right = self.binary_search(nums[right:], target)
+                if new_right != -1:
+                    right += new_right
+                else:
+                    break
+            return [left, right]
+
+    def binary_search(self, nums: List[int], target: int) -> int:
+        """
+        T: O(log(n))
+        
+        :param nums: The array of numbers
+        :param target: The target value to search for
+        :return: the index of the target value
+        """
+        if not nums or (len(nums) == 1 and nums[0] != target):
+            return -1
+        mid = len(nums) // 2
+        if nums[mid] == target:
+            return mid
+        else:
+            if target < nums[mid]:
+                return self.binary_search(nums[: mid], target)
+            else:
+                idx = self.binary_search(nums[mid + 1:], target)
+                return mid + idx + 1 if idx != -1 else -1
 
     def brute_force(self, nums: List[int], target: int) -> List[int]:
         """
@@ -46,4 +106,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    Solution().searchRange([5, 7, 7, 8, 8, 10], 8)
+    print(Solution().searchRange([1, 2, 3], 3))
