29195558b1c72484ab06703518d39f5cb90bfd0d
Dxyk <danielkong1997@gmail.com>
2019-05-26 12:18:53 -0400

    [043] done

--- Python/043-multiply-strings.py
+++ Python/043-multiply-strings.py
@@ -1,4 +1,6 @@
 class Solution:
+    ZERO_ORD = 48
+
     def multiply(self, num1: str, num2: str) -> str:
         """
         Given two non-negative integers num1 and num2 represented as strings,
@@ -13,23 +15,95 @@ class Solution:
         :param num2: the second number in string
         :return: the product of num1 and num2 in string
         """
-        return self.built_in_soln(num1, num2)
+        return self.array_representation_soln(num1, num2)
 
-    def my_soln(self, num1: str, num2: str) -> str:
+    def array_representation_soln(self, num1: str, num2: str) -> str:
         """
-        using python's built-ins
+        https://leetcode.com/problems/multiply-strings/discuss/17605
 
         :param num1: the first number in string
         :param num2: the second number in string
         :return: the product of num1 and num2 in string
         """
-        zero_ord = 48
         if num1 == '0' or num2 == '0':
             return '0'
-        for i in range(max(len(num1), len(num2))):
 
+        res = ""
+        pos = [0] * (len(num1) + len(num2))
 
+        for i in range(len(num1) - 1, -1, -1):
+            n1 = self._str_to_num(num1[i])
+            for j in range(len(num2) - 1, -1, -1):
+                n2 = self._str_to_num(num2[j])
+                mul = n1 * n2
+                p1, p2 = i + j, i + j + 1
+                curr_sum = mul + pos[p2]
 
+                pos[p1] += curr_sum // 10
+                pos[p2] = curr_sum % 10
+
+        for i in range(len(pos)):
+            if not (i == 0 and pos[i] == 0):
+                res += self._num_to_str(pos[i])
+        return res
+
+    def my_soln(self, num1: str, num2: str) -> str:
+        """
+        My method: loop through each digit
+
+        :param num1: the first number in string
+        :param num2: the second number in string
+        :return: the product of num1 and num2 in string
+        """
+        res = ""
+        if num1 == '0' or num2 == '0':
+            return '0'
+
+        rows = []
+        for i in range(len(num1) - 1, -1, -1):
+            n1 = self._str_to_num(num1[i])
+            prev_carry = 0
+            curr_res = 0
+            for j in range(len(num2) - 1, -1, -1):
+                n2 = self._str_to_num(num2[j])
+                product = n1 * n2 + prev_carry
+                carry = product // 10
+                digit = product - carry * 10
+                curr_res += digit * (10 ** (len(num2) - j - 1))
+                prev_carry = carry
+            curr_res += prev_carry * (10 ** len(num2))
+            rows.append(curr_res)
+
+        i = 0
+        res_num = 0
+        while i < len(rows):
+            res_num += rows[i] * (10 ** i)
+            i += 1
+
+        while res_num:
+            nxt = res_num // 10
+            digit = res_num - nxt * 10
+            res = self._num_to_str(digit) + res
+            res_num = nxt
+        return res
+
+    def _num_to_str(self, num: int) -> str:
+        """
+        Helper function that turns a number to a string
+
+        :param num: the number
+        :return: the string
+        """
+        return chr(num + self.ZERO_ORD)
+
+    def _str_to_num(self, string: str) -> int:
+        """
+        Helper function that turns a string to a number
+
+        :param string: the string
+        :return: the number
+        """
+        return ord(string) - self.ZERO_ORD
 
     def built_in_soln(self, num1: str, num2: str) -> str:
         """
