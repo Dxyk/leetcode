3706c5a7bcb5d591e5c1adf85eb8b99e05fa4c06
Dxyk <danielkong1997@gmail.com>
2019-06-09 21:22:07 -0400

    [049] done

--- Python/049-group-anagrams.py
+++ Python/049-group-anagrams.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Tuple
 
 
 class Solution:
@@ -20,16 +20,71 @@ class Solution:
         """
         if not strs:
             return []
-        return self.brute_force(strs)
+        return self.hash_map_letter_count_soln(strs)
 
-    def hash_map_soln(self, strs: List[str]) -> List[List[str]]:
+    def hash_map_letter_count_soln(self, strs: List[str]) -> List[List[str]]:
         """
-        T: O(n^2 * k^2)
-        the brute force soln
+        T: O(n * k)
+        where
+        - n is the length of strs
+        - k is the max length of word in strs
+
+        The hash map solution
+        The map: (countOfEachCharacter) : [strs anagrams]
+
+        e.g. "aaabbc"
+        {(3, 2, 1, 0, ..., 0) : ["aaabbc"]}
 
         :param strs: an array of strings
         :return: a list of groups of anagrams
         """
+        d: Dict[Tuple[int], List[str]] = {}
+        head = ord("a")
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - head] += 1
+            if tuple(count) in d:
+                d[tuple(count)].append(word)
+            else:
+                d[tuple(count)] = [word]
+        return list(d.values())
+
+
+    def hash_map_sorted_letters_soln(self, strs: List[str]) -> List[List[str]]:
+        """
+        T: O(n * k * log(k))
+        where
+        - n is the length of strs
+        - k is the max length of word in strs
+
+        The hash map solution
+        The map: sortedLetters : [strs anagrams]
+
+        :param strs: an array of strings
+        :return: a list of groups of anagrams
+        """
+        d: Dict[str, List[str]] = {}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word in d:
+                d[sorted_word].append(word)
+            else:
+                d[sorted_word] = [word]
+
+        return list(d.values())
+
+    def _sort_word(self, word: str) -> str:
+        """
+        Helper method that sorts a word
+
+        :param word: the unsorted word
+        :return: the sorted word
+        """
+        ords = [ord(ch) for ch in word]
+        ords.sort()
+        return "".join([chr(code) for code in ords])
 
     def brute_force(self, strs: List[str]) -> List[List[str]]:
         """
