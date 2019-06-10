8c0721ad2c2bc9b3cd6e487080787baa4fb600ab
Dxyk <danielkong1997@gmail.com>
2019-06-09 20:31:07 -0400

    [047] done - need revisiting

--- Python/047-permutations-ii.py
+++ Python/047-permutations-ii.py
@@ -17,7 +17,7 @@ class Solution:
             return []
         return self.dfs_recursive_soln(nums)
 
-    def dfs_recursive_soln(self, nums: List[int]) -> List[List[int]]:
+    def dfs_iterative_soln(self, nums: List[int]) -> List[List[int]]:
         """
         The DFS soln
 
@@ -26,40 +26,49 @@ class Solution:
         """
         if not nums:
             return []
+        # O(n*log(n))
         nums.sort()
-        ret = [[]]
+        results = [[]]
         for num in nums:
-            new_ret = []
-            curr_seq_len = len(ret[-1])
-            for seq in ret:
+            curr_results = []
+            curr_seq_len = len(results[-1])
+            for res in results:
                 for i in range(curr_seq_len, -1, -1):
-                    if i < curr_seq_len and seq[i] == num:
+                    if i < curr_seq_len and res[i] == num:
                         break
-                    new_ret.append(seq[:i] + [num] + seq[i:])
-            ret = new_ret
-        return ret
+                    curr_results.append(res[:i] + [num] + res[i:])
+            results = curr_results
+        return results
 
-    def dfs(self, nums: List[int], visited: List[bool], path: List[int],
-            res: List[List[int]]) -> None:
+    def dfs_recursive_soln(self, nums: List[int]) -> List[List[int]]:
         """
-        The DFS backtracking
-        :param nums:
-        :param visited:
-        :param path:
-        :param res:
-        :return:
+        The DFS recursive soln
+
+        :param nums: a list of numbers that might contain duplicates
+        :return: a list of all permutations
         """
-        if len(nums) == len(path):
+        res = []
+        nums.sort()
+        self.dfs_recursive(nums, [], res)
+        return res
+
+    def dfs_recursive(self, nums: List[int], path: List[int],
+                      res: List[List[int]]) -> None:
+        """
+        The DFS recursive helper given the numbers are sorted
+
+        :param nums: the list of numbers
+        :param path: the current path
+        :param res: the result
+        :return: None
+        """
+        if not nums:
             res.append(path)
-            return
         for i in range(len(nums)):
-            if not visited[i]:
-                if i > 0 and not visited[i - 1] and nums[i] == nums[i - 1]:
-                    continue
-                visited[i] = True
-                self.dfs(nums, visited, path + [nums[i]], res)
-                visited[i] = False
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.dfs_recursive(nums[:i] + nums[i + 1:], path + [nums[i]], res)
 
 
 if __name__ == '__main__':
-    Solution().permuteUnique([1, 1, 2])
+    Solution().permuteUnique([1, 2, 1, 1, 1])
