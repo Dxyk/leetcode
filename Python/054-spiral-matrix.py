122c79774e7cf1faa83ece56e10678507ff6b7c7
Dxyk <danielkong1997@gmail.com>
2019-06-11 16:05:20 -0400

    [054] wip

---
+++ Python/054-spiral-matrix.py
@@ -0,0 +1,56 @@
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        """
+        Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in
+        spiral order.
+
+        >>> Solution().spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        >>> Solution().spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+        [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+
+        :param matrix: the matrix
+        :return: spiral traversal of the matrix
+        """
+        if not matrix:
+            return []
+        return self.matrix_rotation_soln(matrix)
+
+    def matrix_rotation_soln(self, matrix: List[List[int]]) -> List[int]:
+        """
+        Each iteration we pop the first row and then rotate the matrix in the reverse direction
+        of the traversal
+
+        :param matrix: the matrix
+        :return: spiral traversal of the matrix
+        """
+
+        def rotate_matrix(orig_matrix: List[List[int]]) -> List[List[int]]:
+            """
+            return the rotated matrix
+
+            :param orig_matrix: the original matrix
+            :return: the rotated matrix
+            """
+            new_rows, new_cols = len(orig_matrix[0]), len(orig_matrix)
+            rotated_matrix = [[0] * new_cols for _ in range(new_rows)]  # [num cols * num rows]
+            for i in range(new_rows):
+                for j in range(new_cols):
+                    rotated_matrix[i][j] = orig_matrix[j][new_cols - i]
+                    print((i, j), orig_matrix, rotated_matrix)
+            return rotated_matrix
+
+        res = []
+        while matrix and matrix[0]:
+            print(matrix)
+            res.extend(matrix[0])
+            matrix.pop(0)
+            matrix = rotate_matrix(matrix)
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
