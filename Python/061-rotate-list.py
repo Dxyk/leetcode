35a8d01970f2b1f4c80bd810ecdb6927bbbc892d
Dxyk <danielkong1997@gmail.com>
2019-07-27 15:02:10 -0400

    [061] done

--- Python/061-rotate-list.py
+++ Python/061-rotate-list.py
@@ -12,28 +12,71 @@ class Solution:
         >>> ll = LinkedList([0, 1, 2])
         >>> print(Solution().rotateRight(ll.get_start(), 4))
         2->0->1
+        >>> ll = LinkedList([1])
+        >>> print(Solution().rotateRight(ll.get_start(), 1))
+        1
 
         :param head: the head node
         :param k: the number of places to rotate right by
         :return: the new head of the list node
         """
-        if not head or not head.val or k < 0:
+        if not head or head.val is None or k <= 0:
             return head
-        return self.brute_force(head, k)
+        return self.two_pointer(head, k)
 
-    def brute_force(self, head: ListNode, k: int) -> ListNode:
+    def two_pointer(self, head: ListNode, k: int) -> ListNode:
         """
         The brute force solution
 
         :param head: the head node
-        :param k: the number of places to rotate right by
+        :param k: the number of places to rotate left by
         :return: the new head of the list node
         """
-        curr = head
+        # count total
+        curr = ListNode(-1)
+        curr.next = head
+        count = 0
+
+        while curr.next:
+            curr = curr.next
+            count += 1
+
+        # mod k so that we only have to loop once
+        k %= count
+        if not k:
+            return head
+
+        # use two pointers to keep track of the last k nodes
+        # left will have the last k nodes in the end
+        # right will be k nodes away from left and will be at the last node in the end
+        left = right = head
+
         while k > 0:
-            if not curr.next:
-                curr = head
-            else:
-                curr = curr.next
+            right = right.next
             k -= 1
+
+        while right.next:
+            left = left.next
+            right = right.next
+        # head -> ... -> left -> left.next -> ... -> right -> None
+
+        temp = left.next
+        # head -> ... -> left -> left.next -> ... -> right -> None
+        # temp (left.next) -> ... -> right -> None
+
+        left.next = None
+        # head -> ... -> left -> None
+        # temp (left.next) -> ... -> right -> None
+
+        right.next = head
+        # temp (left.next) -> ... -> head -> ... -> left -> None
+
+        head = temp
+        # head (temp) (left.next) -> ... -> old_head -> ... -> left -> None
+
         return head
+
+
+if __name__ == '__main__':
+    ll = LinkedList([0, 1, 2])
+    print(Solution().rotateRight(ll.get_start(), 4))
