e1290d460dde1ecc58964812e026af6db5ee8f43
Dxyk <danielkong1997@gmail.com>
2019-12-25 18:25:01 -0500

    42 trapping rain water done

--- Python/42.trapping-rain-water.py
+++ Python/42.trapping-rain-water.py
@@ -35,5 +35,111 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        return self.two_pointer_soln(height)
+
+    def two_pointer_soln(self, height: List[int]) -> int:
+        """
+        Two pointer solution
+
+        Runtime: O(n)
+        Space: O(1)
+        """
+        res = 0
+        left, right = 0, len(height) - 1
+        left_max = right_max = 0
+        while left < right:
+            curr_left, curr_right = height[left], height[right]
+            if curr_left < curr_right:
+                if curr_left > left_max:
+                    left_max = curr_left
+                else:
+                    res += left_max - curr_left
+                left += 1
+            else:
+                if curr_right > right_max:
+                    right_max = curr_right
+                else:
+                    res += right_max - curr_right
+                right -= 1
+        return res
+
+    def stack_soln(self, height: List[int]) -> int:
+        """
+        Stack solution
+
+        By using a stack, only 1 loop is needed
+
+        Runtime: O(n)
+        Space: O(n)
+        """
+        res = 0
+        stack = []
+        for i, curr_height in enumerate(height):
+            while len(stack) > 0 and height[stack[-1]] < curr_height:
+                left_bottom_height = height[stack.pop()]
+                if len(stack) == 0:
+                    break
+                left_upper_idx = stack[-1]
+                left_upper_height = height[left_upper_idx]
+                diff = min(left_upper_height, curr_height) - left_bottom_height
+                width = i - left_upper_idx - 1
+                res += width * diff
+            stack.append(i)
+        return res
+
+    def dp_soln(self, height: List[int]) -> int:
+        """
+        DP solution
+
+        Store the max_left, max_right idx so that we don't need
+        to recompute them every iteration compared to bf soln
+
+        Runtime: O(n)
+        Space: O(n)
+        """
+        if len(height) == 0:
+            return 0
+
+        res = 0
+        max_lefts = [0] * len(height)
+        max_rights = [0] * len(height)
+
+        max_lefts[0] = height[0]
+        for i in range(1, len(height)):
+            max_lefts[i] = max(height[i], max_lefts[i - 1])
+
+        max_rights[-1] = height[-1]
+        for i in range(len(height) - 2, -1, -1):
+            max_rights[i] = max(height[i], max_rights[i + 1])
+
+        for i in range(len(height)):
+            max_left, max_right = max_lefts[i], max_rights[i]
+            res += max(min(max_left, max_right) - height[i], 0)
+
+        return res
+
+    def brute_force(self, height: List[int]) -> int:
+        """
+        brute force solution
+
+        Runtime: O(n^2)
+        Space: O(1)
+        """
+        if len(height) == 0:
+            return 0
+
+        res = 0
+        for i in range(len(height)):
+            max_left = max_right = 0
+            for j in range(i):
+                max_left = max(height[j], max_left)
+            for j in range(i + 1, len(height)):
+                max_right = max(height[j], max_right)
+            res += max(min(max_left, max_right) - height[i], 0)
+        return res
+
 # @lc code=end
+
+
+if __name__ == "__main__":
+    print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6)
