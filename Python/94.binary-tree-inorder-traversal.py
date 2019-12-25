6f67ac8ef19e26e7dde445a89d6b14b5960c934c
Dxyk <danielkong1997@gmail.com>
2019-12-24 21:15:43 -0500

    94 binary tree inorder traversal init

---
+++ Python/94.binary-tree-inorder-traversal.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+# https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+#
+# algorithms
+# Medium (59.85%)
+# Likes:    2249
+# Dislikes: 96
+# Total Accepted:    586.6K
+# Total Submissions: 979.6K
+# Testcase Example:  '[1,null,2,3]'
+#
+# Given a binary tree, return the inorder traversal of its nodes' values.
+#
+# Example:
+#
+#
+# Input: [1,null,2,3]
+# ⁠  1
+# ⁠   \
+# ⁠    2
+# ⁠   /
+# ⁠  3
+#
+# Output: [1,3,2]
+#
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        pass
+# @lc code=end
