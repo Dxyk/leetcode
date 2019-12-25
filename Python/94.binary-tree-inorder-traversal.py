e021411e19cbe2f14e0022fa74f579dd38c302da
Dxyk <danielkong1997@gmail.com>
2019-12-24 21:35:43 -0500

    binary tree traversal

--- Python/94.binary-tree-inorder-traversal.py
+++ Python/94.binary-tree-inorder-traversal.py
@@ -45,5 +45,70 @@ class TreeNode:
 
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        pass
+        return self.morris_soln(root)
+
+    def morris_soln(self, root: TreeNode) -> List[int]:
+        """
+        Morris Traversal
+        (https://leetcode.com/problems/binary-tree-inorder-traversal/solution/)
+
+        Utilizes Threaded Binary Tree by reforming the tree to form a
+        single-child tree
+        """
+        res = []
+        curr = root
+        while curr is not None:
+            if curr.left is None:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                prev = curr.left
+                while prev.right is not None:
+                    prev = prev.right
+                prev.right = curr
+                tmp = curr
+                curr = curr.left
+                tmp.left = None
+        return res
+
+    def iterative_soln(self, root: TreeNode) -> List[int]:
+        """
+        Iteratively traverse the binary tree
+
+        This traversal uses a stack because stack provides us
+        a way to backtrack
+        """
+        res = []
+        stack = []
+        curr = root
+        while curr is not None or len(stack) != 0:
+            while curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+
+    def recursive_soln(self, root: TreeNode) -> List[int]:
+        """
+        Recursively inorder traverse the binary tree
+        """
+        res = []
+
+        if root is None:
+            return res
+        res.extend(self.recursive_soln(root.left))
+        res.append(root.val)
+        res.extend(self.recursive_soln(root.right))
+
+        return res
+
 # @lc code=end
+
+
+if __name__ == "__main__":
+    node = TreeNode(1)
+    node.right = TreeNode(2)
+    node.right.left = TreeNode(3)
+    print(Solution().inorderTraversal(node))
